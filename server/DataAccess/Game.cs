6e258ad6c1159c8430fd35d394495ef563a0c646
odrymark <odrymark@gmail.com>
2025-11-24 10:39:56 +0100

    Added seeder, boards only able to be bought until Saturday 5pm

--- server/DataAccess/Game.cs
+++ server/DataAccess/Game.cs
@@ -7,6 +7,7 @@ public class Game
     public ICollection<User> winners { get; set; } = new List<User>();
     public int income { get; set; }
     public DateTime createdAt { get; set; }
+    public DateTime openUntil { get; set; }
     public ICollection<Board> boards { get; set; } = new List<Board>();
 
 }
